6958c978d73e4401ea0b25a06fd0925b24099e17
nov144 <nov144@mail.ru>
2025-05-28 07:31:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -89,6 +89,27 @@ app.router.add_post("/webhook", dp.webhook_handler)
 app.on_startup.append(on_startup)
 app.on_shutdown.append(on_shutdown)
 
+from aiogram.utils.executor import start_webhook
+
+WEBHOOK_HOST = os.getenv("WEBHOOK_URL")
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
 if __name__ == "__main__":
-    web.run_app(app, port=10000)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
+
 
