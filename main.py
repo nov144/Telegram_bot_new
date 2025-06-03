2105f5a1a65509b0a94137f0f11e490311501c7e
nov144 <nov144@mail.ru>
2025-06-03 20:11:29 +1000

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def print_webhook_info(app: web.Application):
 app = web.Application()
 SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path="/webhook")
 setup_application(app, dp, bot=bot)
-app.on_startup.append(set_webhook)
+app.on_startup.append(on_startup)
 app.on_startup.append(print_webhook_info)
 
 
